c21b7914bcf577dc105e21b5b23190db93146b39
Anvarys <105349498+Anvarys@users.noreply.github.com>
2026-03-08 21:38:20 +0100

    fix: remove unused import

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { Label } from "@/components/ui/label";
 
 export function Timer() {
